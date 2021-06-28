fcc8ad1f6edaf2931251c5e35ec2ed89c58204f2
Anton Serdyuchenko <anton415@gmail.com>
2021-06-28 12:17:59 +0300

    2.1. Реализация класса StartUI [#784 #172017]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,116 +4,17 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class StartUI {
-    public static final Pattern PATTERN_FOR_MENU = Pattern.compile("[0-6]");
-    public static final Pattern PATTERN_FOR_ID = Pattern.compile("^\\d+$");
 
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
-            this.showMenu();
+            showMenu();
             System.out.print("Select: ");
-            String selectedMenu = scanner.nextLine();
-            if (isMenuSelectCorrect(selectedMenu)) {
-                int select = Integer.parseInt(selectedMenu);
-                System.out.println("Пользователь выбрал: " + select);
-                if (select == 0) {
-                    addNewItem(scanner, tracker);
-                } else if (select == 1) {
-                    showAllItems(tracker);
-                } else if (select == 2) {
-                    editItem(scanner, tracker);
-                } else if (select == 3) {
-                    deleteItem(scanner, tracker);
-                } else if (select == 4) {
-                    findItemById(scanner, tracker);
-                } else if (select == 5) {
-                    findItemByName(scanner, tracker);
-                } else if (select == 6) {
-                    run = false;
-                }
-            } else {
-                System.out.println("Error 404. Menu not found!");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select == 6) {
+                run = false;
             }
-
         }
-    }
-
-    private void findItemByName(Scanner scanner, Tracker tracker) {
-        System.out.print("Enter id: ");
-        String name = scanner.nextLine();
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Error 404. Items not found!");
-        }
-    }
-
-    private void findItemById(Scanner scanner, Tracker tracker) {
-        System.out.print("Enter id: ");
-        String id = scanner.nextLine();
-        if (isIdEnterCorrect(id)) {
-            Item item = tracker.findById(Integer.parseInt(id));
-            System.out.println(item);
-        } else {
-            System.out.println("Error 404. Id not found!");
-        }
-    }
-
-    private void deleteItem(Scanner scanner, Tracker tracker) {
-        System.out.print("Enter id: ");
-        String id = scanner.nextLine();
-        if (isIdEnterCorrect(id)) {
-            tracker.delete(Integer.parseInt(id));
-        } else {
-            System.out.println("Error 404. Id not found!");
-        }
-
-    }
-
-    private void editItem(Scanner scanner, Tracker tracker) {
-        System.out.println("=== Edit item ====");
-        System.out.print("Enter id: ");
-        int id = Integer.parseInt(scanner.nextLine());
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        Item item = new Item(name);
-        if (tracker.replace(id, item)) {
-            System.out.println("Заявка изменена успешно.");
-        } else {
-            System.out.println("Ошибка замены заявки.");
-        }
-    }
-
-    private void showAllItems(Tracker tracker) {
-        System.out.println("=== Show all items ====");
-        Item[] items = tracker.findAll();
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Хранилище еще не содержит заявок");
-        }
-    }
-
-    private void addNewItem(Scanner scanner, Tracker tracker) {
-        System.out.println("=== Create a new Item ====");
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        Item item = new Item(name);
-        tracker.add(item);
-        System.out.println("Добавленная заявка: " + item);
-    }
-
-    private boolean isMenuSelectCorrect(String id) {
-        return id != null && PATTERN_FOR_MENU.matcher(id).matches();
-    }
-
-    private boolean isIdEnterCorrect(String id) {
-        return id != null && PATTERN_FOR_ID.matcher(id).matches();
     }
 
     private void showMenu() {
