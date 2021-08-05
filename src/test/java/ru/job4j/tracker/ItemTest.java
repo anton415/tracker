243fc56585d8302c9b427ffbbd4ecb031ff4ec8a
Anton Serdyuchenko <anton415@gmail.com>
2021-08-05 13:41:52 +0300

    0. Сортировка [#10096 #185060]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.tracker;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,19 +10,20 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ItemTest {
-    List<Item> items = new ArrayList<>();
-    List<Item> itemsSort = new ArrayList<>();
-    List<Item> itemsReversed = new ArrayList<>();
-
-    @Before
-    public void before() {
-        initNotSortedList();
-        initSortedList();
-        initReversedList();
-    }
-
     @Test
     public void whenSortUp() {
+        List<Item> items = new ArrayList<>();
+        List<Item> itemsSort = new ArrayList<>();
+        items.add(new Item("Abc"));
+        items.add(new Item("zxcf"));
+        items.add(new Item(""));
+        items.add(new Item("123"));
+        items.add(new Item("abc"));
+        itemsSort.add(items.get(2));
+        itemsSort.add(items.get(3));
+        itemsSort.add(items.get(0));
+        itemsSort.add(items.get(4));
+        itemsSort.add(items.get(1));
         Collections.sort(items, new SortItem());
         System.out.println(items);
         assertThat(items, is(itemsSort));
@@ -31,32 +31,19 @@ public class ItemTest {
 
     @Test
     public void whenSortDown() {
-        Collections.sort(items, new ReversedItem());
-        System.out.println(items);
-    }
-
-    private void initNotSortedList() {
+        List<Item> items = new ArrayList<>();
+        List<Item> itemsReversed = new ArrayList<>();
         items.add(new Item("Abc"));
         items.add(new Item("zxcf"));
         items.add(new Item(""));
         items.add(new Item("123"));
         items.add(new Item("abc"));
-    }
-
-    private void initSortedList() {
-        itemsSort.add(items.get(2));
-        itemsSort.add(items.get(3));
-        itemsSort.add(items.get(0));
-        itemsSort.add(items.get(4));
-        itemsSort.add(items.get(1));
-    }
-
-    private void initReversedList() {
         itemsReversed.add(items.get(1));
         itemsReversed.add(items.get(4));
         itemsReversed.add(items.get(0));
         itemsReversed.add(items.get(3));
         itemsReversed.add(items.get(2));
+        Collections.sort(items, new ReversedItem());
+        System.out.println(items);
     }
-
 }
