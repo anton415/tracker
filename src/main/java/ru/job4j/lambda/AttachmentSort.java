07e99db1b75154eb6abfde5fb636a003eec6fec2
Anton Serdyuchenko <anton415@gmail.com>
2021-09-17 22:13:56 +0300

    Чистка проекта.

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -11,25 +11,11 @@ public class AttachmentSort {
                 new Attachment("image 2", 34),
                 new Attachment("image 3", 13)
         );
-        Comparator comparator =  new Comparator() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                Attachment left = (Attachment) o1;
-                Attachment right = (Attachment) o2;
-                return left.getSize() - right.getSize();
-            }
-        };
+        Comparator<Attachment> comparator = Comparator.comparingInt(Attachment::getSize);
         attachments.sort(comparator);
         System.out.println(attachments);
 
-        Comparator comparatorByName =  new Comparator() {
-            @Override
-            public int compare(Object o1, Object o2) {
-                Attachment left = (Attachment) o1;
-                Attachment right = (Attachment) o2;
-                return left.getName().compareTo(right.getName());
-            }
-        };
+        Comparator<Attachment> comparatorByName = Comparator.comparing(Attachment::getName);
         attachments.sort(comparatorByName);
         System.out.println(attachments);
     }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tracker {
-    private final ArrayList<Item> items = new ArrayList();
+    private final ArrayList<Item> items = new ArrayList<>();
     private int ids = 1;
 
     public boolean delete(int id) {
