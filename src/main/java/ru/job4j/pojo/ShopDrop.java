851dd529b2704d288b33470c62889cd98870c630
Anton Serdyuchenko <anton415@gmail.com>
2021-10-26 19:33:48 +0300

    For validation.

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -2,8 +2,9 @@ package ru.job4j.pojo;
 
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
-        if (products.length - 1 - index >= 0)
+        if (products.length - 1 - index >= 0) {
             System.arraycopy(products, index + 1, products, index, products.length - 1 - index);
+        }
         products[products.length - 1] = null;
         return products;
     }
